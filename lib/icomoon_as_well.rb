136d6dd53a15e115f6e07ad981e78cbbda34e56d
Alex <babakhanov1@gmail.com>
2015-11-08 12:31:36 +0200

    parse icomoon variables

--- lib/icomoon_as_well.rb
+++ lib/icomoon_as_well.rb
@@ -12,6 +12,7 @@ module IcomoonAsWell
     def initialize(path, target_folder)
       @archive = Unzip.new(path)
       put_files(@archive.files, @archive.files.keys.map{|name| $1 if name =~ /^fonts\/(.+)$/}.compact, "#{target_folder}/fonts")
+      parse_css(@archive.files["style.css"])
     end
   end
 end
--- lib/icomoon_as_well/file_helper.rb
+++ lib/icomoon_as_well/file_helper.rb
@@ -1,5 +1,23 @@
 module IcomoonAsWell
   module FileHelper
+    def parse_css(file)
+      text = ""
+			c = 0
+			lines = file.split("\n")
+			while c <= lines.count
+				line = lines[c]
+				c += 1
+				if line.to_s =~ /^\.icon-(\S+):before\s*{\s*$/
+					icon_name = "\$icon-#{$1}: "
+					value = lines[c]
+					c += 1
+					icon_name.gsub!(/\s+/, '')
+          value.gsub!(/^\s+/, '').gsub!(/content:/, '').gsub!(/["'\\\s\;]+/, '')
+					text << "#{icon_name} '\\#{value}';\n"
+				end
+      end
+			puts text
+    end
     def put_files(entry, names, target_dir)
       unless Dir.exist?(target_dir)
         Dir.mkdir(target_dir)
