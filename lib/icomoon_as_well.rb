542b0c25764856c536a047d3252240642459ab5b
Alex <babakhanov1@gmail.com>
2015-11-08 16:07:08 +0200

    add core css

--- lib/icomoon_as_well.rb
+++ lib/icomoon_as_well.rb
@@ -11,10 +11,11 @@ module IcomoonAsWell
       put_files(@archive.files, @archive.files.keys.map{|name| $1 if name =~ /^fonts\/(.+)$/}.compact, "#{target_dir}/fonts")
       @icomoon_variables = parse_css(@archive.files["style.css"])
       @icon_prefix = "icon"
-      ["variables", "icons", "core", "mixins"].each do |tpl|
+      ["variables", "icons", "core", "mixins", "main"].each do |tpl|
         renderer = ::ERB.new File.read("#{File.dirname(__FILE__)}/icomoon_as_well/templates/#{tpl}.scss.erb")
         File.open(File.join(icomoon_dir(target_dir), "#{tpl}.scss"), "w"){|f| f.write(renderer.result(binding)) }
       end
     end
   end
 end
+
--- lib/icomoon_as_well/file_helper.rb
+++ lib/icomoon_as_well/file_helper.rb
@@ -2,21 +2,21 @@ module IcomoonAsWell
   module FileHelper
     def parse_css(file)
       icons = {}
-			c = 0
-			lines = file.split("\n")
-			while c <= lines.count
-				line = lines[c]
-				c += 1
-				if line.to_s =~ /^\.icon-(\S+):before\s*{\s*$/
-					icon_name = $1
-					value = lines[c]
-					c += 1
-					icon_name.gsub!(/\s+/, '')
+      c = 0
+      lines = file.split("\n")
+      while c <= lines.count
+        line = lines[c]
+        c += 1
+        if line.to_s =~ /^\.icon-(\S+):before\s*{\s*$/
+          icon_name = $1
+          value = lines[c]
+          c += 1
+          icon_name.gsub!(/\s+/, '')
           value.gsub!(/^\s+/, '').gsub!(/content:/, '').gsub!(/["'\\\s\;]+/, '')
           icons[icon_name] = value
-				end
+        end
       end
-			icons
+      icons
     end
     def put_files(entry, names, target_dir)
       unless Dir.exist?(target_dir)
